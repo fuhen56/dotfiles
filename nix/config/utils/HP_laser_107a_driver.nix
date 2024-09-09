b38d6ffb87b0c8822799b6f3f069943e6b70d17b
James <theanos@tutanota.com>
2024-09-09 20:13:52 +0100

    cleaned the derivation

--- nix/config/utils/HP_laser_107a_driver.nix
+++ nix/config/utils/HP_laser_107a_driver.nix
@@ -1,14 +1,12 @@
 {
-  lib,
   fetchurl,
   stdenv,
-  fetchzip,
 }: let
   model = "HP_Laser_10x_Series.ppd";
-  archiveUrl = "https://ftp.hp.com/pub/softlib/software13/printers/CLP150/uld-hp_V1.00.39.12_00.15.tar.gz"; # Replace with the actual URL of the archive
+  archiveUrl = "https://ftp.hp.com/pub/softlib/software13/printers/CLP150/uld-hp_V1.00.39.12_00.15.tar.gz";
   archiveSha256 = "cebb9b7b6125e7406634bb9c2a98b01477d1e11d66c7c90474669de9927bc91d";
 in
-  stdenv.mkDerivation rec {
+  stdenv.mkDerivation {
     name = "hp-laser-107a";
 
     src = fetchurl {
@@ -18,10 +16,10 @@ in
 
     installPhase = ''
       mkdir -p $out/share/cups/HP/
-      tar -xzf $src --wildcards "*HP_Laser_10x_Series*" --strip-components=4 -C $out/share/cups/HP-addition/$model
+      tar -xzf $src --wildcards "*HP_Laser_10x_Series*" --strip-components=4 -C $out/share/cups/HP-addition/${model}
     '';
 
-    meta = with lib; {
+    meta = {
       description = "HP Laser 107a Printer Driver";
     };
   }
